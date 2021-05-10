d37a50d635ebc8bc9d2a23a107d5cb5ba269bd91
Shakran  Bhat <shakranbhat@Shakrans-MacBook-Pro.local>
2021-05-10 20:39:13 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+
+const connectDB = require('./config/db')
+const app = express()
+
+// Connect Database
+connectDB()
+
+
+
+// init middleware
+
+app.use(express.json({extended:false}))
+
+
+app.get('/', (req, res) => {
+    res.send('API running')
+})
+
+
+app.use('/api/users', require('./routes/api/users'))
+app.use('/api/auth', require('./routes/api/auth'))
+app.use('/api/profile', require('./routes/api/profile'))
+app.use('/api/posts', require('./routes/api/posts'))
+
+const PORT  =  process.env.PORT || 5000
+
+app.listen(PORT, () => console.log('Server is runnning on port ' +PORT))
