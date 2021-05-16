172f331b8f490b89b5f205b28e788b4117bc1c47
bhatshakran <bhatshakran@gmail.com>
2021-05-16 19:15:15 +0530

    added timeout param

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -2,12 +2,12 @@ import {v4 as uuidv4} from 'uuid'
 import {SET_ALERT, REMOVE_ALERT} from './types'
 
 
-export const setAlert = (msg, alertType) => dispatch => {
+export const setAlert = (msg, alertType, timeout =3000) => dispatch => {
     const id = uuidv4()
     dispatch({
         type:SET_ALERT,
         payload: {msg, alertType, id}
     })
 
-    setTimeout(()=> dispatch({ type: REMOVE_ALERT, payload:id }), 3000)
+    setTimeout(()=> dispatch({ type: REMOVE_ALERT, payload:id }), timeout)
 }
