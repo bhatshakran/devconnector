a92d356900a694d451b872752106e552306193db
Shakran  Bhat <shakranbhat@Shakrans-MacBook-Pro.local>
2021-05-16 11:42:42 +0530

    Made routes using react router dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,25 @@
 
 import './App.css';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
 import Landing from './components/layout/Landing'
+import Login from './components/auth/Login'
+import Register from './components/auth/Register'
 
 function App() {
   return (
-    <div className="App">
+    <Router >
+    
      <Navbar/>
-     <Landing />
-    </div>
+     <Route exact path='/' component={Landing} />
+     <section className='container'>
+    <Switch>
+      <Route exact path='/register' component={Register} />
+      <Route exact path='/login' component={Login} />
+    </Switch>
+     </section>
+ 
+    </Router>
   );
 }
 
