b2ce7f565c09efb6930c071f43f62121cdb7cbed
bhatshakran <bhatshakran@gmail.com>
2021-05-20 12:06:25 +0530

    Final production commit

--- server.js
+++ server.js
@@ -1,28 +1,31 @@
-const express = require('express')
-
-const connectDB = require('./config/db')
-const app = express()
+const express = require("express");
+const connectDB = require("./config/db");
+const app = express();
+const path = require("path");
 
 // Connect Database
-connectDB()
-
-
+connectDB();
 
 // init middleware
 
-app.use(express.json({extended:false}))
+app.use(express.json({ extended: false }));
 
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
 
-app.get('/', (req, res) => {
-    res.send('API running')
-})
+// Serve static assets in production
 
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
 
-app.use('/api/users', require('./routes/api/users'))
-app.use('/api/auth', require('./routes/api/auth'))
-app.use('/api/profile', require('./routes/api/profile'))
-app.use('/api/posts', require('./routes/api/posts'))
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
-const PORT  =  process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log('Server is runnning on port ' +PORT))
+app.listen(PORT, () => console.log("Server is runnning on port " + PORT));
